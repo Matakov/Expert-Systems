0b849c854c53d3f1763ab4d73134dc494c958cc4
sabzykoo <sermiskulin@gmail.com>
2017-12-08 23:53:13 +0100

    gui extras, to be continued

--- gui.py
+++ gui.py
@@ -1,8 +1,6 @@
 import sys
 import os
 import subprocess
-from capturer import CaptureOutput
-from cStringIO import StringIO
 
 if sys.version_info[0] < 3:
     # python 2
@@ -10,7 +8,6 @@ if sys.version_info[0] < 3:
     import tkFileDialog as filedialog
     import ttk
     import tkMessageBox as mBox
-    # import clips6
     import clips
     clips.Reset()
 
@@ -21,40 +18,43 @@ else:
     from tkinter import ttk
     from tkinter import messagebox as mBox
 
+
 def GetValues(filename='rulez.clp'):
-	f = open(filename, 'r')
+    f = open(filename, 'r')
 
-	flagLHS = False
-	flagRHS = False
+    flagLHS = False
+    flagRHS = False
 
-	LHS = []
-	RHS = []
-	for line in f:
-		#if line not in ['\n', '\r\n']:
-		#	print line[0]+line[1]
-		if "=>" in line:
-			flagLHS = False
-		if flagLHS:
-			#print line
-			LHS.append(line.strip()[1:-1])	
-		if "defrule" in line:
-			flagLHS = True
+    LHS = []
+    RHS = []
 
-		if "defrule" in line:
-			flagRHS = False
-		if flagRHS:
-			#print line
-			line = line.strip()
-			index = line.find(")))")
-			if index==-1:
-				index = line.find("))")
-			if line != "":
-				RHS.append(line[9:index])	
-		if "=>" in line:
-			flagRHS = True
-		#re.search(r'\s[\s]*\s', x, re.S)
-	f.close()
-	return LHS,RHS
+    for line in f:
+        # if line not in ['\n', '\r\n']:
+        # print line[0]+line[1]
+        if "=>" in line:
+            flagLHS = False
+        if flagLHS:
+            # print line
+            LHS.append(line.strip()[1:-1])
+        if "defrule" in line:
+            flagLHS = True
+
+        if "defrule" in line:
+            flagRHS = False
+        if flagRHS:
+            # print line
+            line = line.strip()
+            index = line.find(")))")
+            if index==-1:
+                index = line.find("))")
+            if line != "":
+                RHS.append(line[9:index])
+        if "=>" in line:
+            flagRHS = True
+        # re.search(r'\s[\s]*\s', x, re.S)
+    f.close()
+    return LHS, RHS
+
 
 # class Window, inheriting from the Frame class.
 class Window(Frame):
@@ -108,33 +108,28 @@ class Window(Frame):
         # Add another Menu to the Menu Bar and an item
         help_menu = Menu(menu, tearoff=1)
         menu.add_cascade(label="Help", menu=help_menu)
-        #help_menu.add_command(label="Presentation", command=self.openPDF("filename.pdf"))
-        #help_menu.add_command(label="Documentation", command=self.openPDF("filename.pdf"))
+        # help_menu.add_command(label="Presentation", command=self.openPDF("prezentacija.pdf"))
+        # help_menu.add_command(label="Documentation", command=self.openPDF("dokumentacija.pdf"))
         help_menu.add_separator()
         help_menu.add_command(label="About", command=self.msgBox)
 
-        Label(self.master, text="Select symptoms:").pack(side=TOP)#.grid(column=0, row=0)
+        Label(self.master, text="Select symptoms:").pack(side=TOP)
 
-        #combo_master = Combos(self.master)
         self.symptom = StringVar()
         self.combo_one = ttk.Combobox(self.master, textvariable=self.symptom, state='readonly')
 
-	LHS,RHS=GetValues('rulez.clp')
-	self.filename='rulez.clp'
-	self.LHS=LHS
-        self.values = list(set(self.LHS))#(1, 2, 3, 4, 5)
+        self.filename = 'rulez.clp'
+
+        LHS, RHS = GetValues(self.filename)
+        self.LHS = LHS
+        self.values = list(set(self.LHS))
         self.combo_one.configure(values=self.values)
         self.combo_one.pack(fill=BOTH, padx=20, pady=10, ipadx=5, ipady=5, side=TOP)
-
-        #self.symptom2 = StringVar()
-        #self.combo_two = ttk.Combobox(self.master, textvariable=self.symptom2, state='disabled')
-        #self.combo_two.configure(values=values)
-        #self.combo_two.pack(fill=BOTH, padx=20, pady=10, ipadx=5, ipady=5, side=TOP)
 
         self.del_button = Button(self.master, text='delete symptom', width=15, height=2,
                           command=lambda: self.remove_simptom(self.combo_one.get()))
         self.del_button.pack()
-	self.run_button = Button(self.master, text='run', width=15, height=2,
+        self.run_button = Button(self.master, text='run', width=15, height=2,
                           command=lambda: self.run())
         self.run_button.pack()
 
@@ -145,69 +140,59 @@ class Window(Frame):
         self.text_area = Text(self.master, state='disable')
         self.text_area.pack()
 
-        value = self.combo_one.get()
+        # value = self.combo_one.get()
         self.combo_one.bind('<<ComboboxSelected>>', self.add_simptom)
-	clips.Load('rulez.clp')
+        clips.Load(self.filename)
 
     def run(self):
-	sys.stdout = open("logfile", "w")
-	self.text_area.configure(state='normal')
+        sys.stdout = open("logfile", "w")
+        self.text_area.configure(state='normal')
         data = self.text_area.get(0.0, END)
         split_data = data.split("\n")
         self.text_area.delete(0.0, END)
         # look if there is already that value inside textbox and if it is, do not add it (remove it)
-        #print(len(split_data))
+        # print(len(split_data))
         for inputs in split_data:
-	    inputs = inputs.encode("utf-8")
-	    if inputs != "":
-		#print inputs
-		if (inputs != "\n"):
-		     clips.Assert("("+inputs+")")
-		     #in2 = " ".join(inputs.split())
-		     #self.text_area.insert(END, inputs.strip() + "\n")
-	split_data_edited = []
-	for line in split_data:
-		line = line.encode("utf-8")
-		if line != "":
-			if (line != "\n"):
-				split_data_edited.append(line)
-		
-	clips.Run()
-	
-	lines = []
-	answer = []
-	clips.PrintFacts()
-	"""
-	with CaptureOutput() as capturer:
-		clips.PrintFacts()
-		lines = capturer.get_lines()
-	"""	
-	#print lines
-	sys.stdout = open("/dev/stdout", "w")
-	f = open("logfile", 'r')
-	for line in f:
-		lines.append(line)
-		
-	for line in lines[1:-1]:
-		#print line
-		line = line.encode("utf-8")
-		index = line.find(")")
-		line = line[9:index]
-		#print line
-		if line not in split_data_edited:
-			answer.append(line)
-	for line in answer:
-		self.text_area.insert(END, line.strip() + "\n")
-	clips.Reset()
-	LHS,RHS=GetValues(self.filename)
-	self.values = list(set(LHS))#(1, 2, 3, 4, 5)
-        self.combo_one.configure(values=self.values)
-	#clips.Assert("(Engine fails to start)")
+            inputs = inputs.encode("utf-8")
+            if inputs != "":
+                # print inputs
+                if inputs != "\n":
+                    clips.Assert("("+inputs+")")
+                    # in2 = " ".join(inputs.split())
+                    # self.text_area.insert(END, inputs.strip() + "\n")
+                split_data_edited = []
+                for line in split_data:
+                    line = line.encode("utf-8")
+                    if line != "":
+                        if line != "\n":
+                            split_data_edited.append(line)
+        clips.Run()
 
-    def on_selection(self, event=None):
-        # self.text_area.insert(END, "Just a text Widget\nin two lines\n")
-        print self.combo_one.get()
-        self.combo_two.configure(state='readonly')
+        lines = []
+        answer = []
+        clips.PrintFacts()
+
+        sys.stdout = open("/dev/stdout", "w")
+        f = open("logfile", 'r')
+        for line in f:
+            lines.append(line)
+
+        for line in lines[1:-1]:
+            # print line
+            line = line.encode("utf-8")
+            index = line.find(")")
+            line = line[9:index]
+            # print line
+            if line not in split_data_edited:
+                answer.append(line)
+        for line in answer:
+            self.text_area.insert(END, line.strip() + "\n")
+        self.text_area.insert(END, "-----------------------------------" + "\n")
+        clips.Reset()
+
+        # LHS, RHS = GetValues(self.filename)
+        # self.values = list(set(LHS))
+        # self.combo_one.configure(values=self.values)
 
     def add_simptom(self, event=None):
         # get data so far in textbox
@@ -219,7 +204,7 @@ class Window(Frame):
         ifIn = 0
         # look if there is already that value inside textbox
         for inputs in split_data:
-            if (inputs == value):
+            if inputs == value:
                 ifIn = 1
         # if value is not in, add it
         # if value is not in, add it
@@ -233,50 +218,26 @@ class Window(Frame):
         self.text_area.configure(state='normal')
         data = self.text_area.get(0.0, END)
         split_data = data.split("\n")
-	#print split_data
+
         self.text_area.delete(0.0, END)
         # look if there is already that value inside textbox and if it is, do not add it (remove it)
-        #print(len(split_data))
+        # print(len(split_data))
         for inputs in split_data:
-	    inputs = inputs.encode("utf-8")
-            if (inputs != value):
-		if inputs != "":
-			#print inputs
-		        if (inputs != "\n"):
-		            #in2 = " ".join(inputs.split())
-		            self.text_area.insert(END, inputs.strip() + "\n")
+            inputs = inputs.encode("utf-8")
+            if inputs != value:
+                if inputs != "":
+                    # print inputs
+                    if inputs != "\n":
+                        # in2 = " ".join(inputs.split())
+                        self.text_area.insert(END, inputs.strip() + "\n")
         self.text_area.configure(state='disable')
-
-    '''
-            # Label(self.master, text="Select your symptom:").grid(column=1, row=0)
-            combo_one = self.addCombo(1, 3, 'readonly')
-            # combo_two = self.addCombo(2, 3, 'disabled', self.changeComboParam(combo_one, self.combo_two))
-            #combo_one.bind('<<ComboboxSelected>>', self.enableCombo)
-            #self.addCombo(2, 3)
-
-            Combos(self.master)
-
-        def changeComboParam(self, combo_en, combo_dis):
-            if combo_en.get() == '2':
-                combo_dis.configure(state='readonly')
-            else:
-                pass
-
-        def addCombo(self, r, c, s, ccp=None):
-            symptom = StringVar()
-            newCombo = ttk.Combobox(self.master, width=40, textvariable=symptom, state=s, postcommand=ccp)
-            newCombo['values'] = (1, 2, 10, 42, 100)
-            newCombo.grid(column=c, row=r)
-            # newCombo.current(0)
-            return newCombo
-    '''
 
     def ruleWin(self):
         gringo = Tk()
-        gringo.geometry("500x500")
+        gringo.geometry("550x550")
 
         # creation of an instance
-        app = ParametersWin(gringo,self)
+        app = ParametersWin(gringo, self)
 
         # mainloop
         gringo.mainloop()
@@ -296,10 +257,10 @@ class Window(Frame):
             [("CLISP files", "*.clp"),
              ("All files", "*.*")]
         filename = filedialog.askopenfilename(filetypes=mask, title="Select file")
-	clips.Load(filename)
-	self.filename=filename
-	LHS,RHS=GetValues(filename)
-	self.values = list(set(LHS))#(1, 2, 3, 4, 5)
+        clips.Load(filename)
+        self.filename=filename
+        LHS,RHS=GetValues(filename)
+        self.values = list(set(LHS))#(1, 2, 3, 4, 5)
         self.combo_one.configure(values=self.values)
 
     def client_save(self):
@@ -315,16 +276,16 @@ class Window(Frame):
                                      filetypes=(("ASCII files", "*.txt"), ("all files", "*.*")))
 
     def client_exit(self):
-	clips.Run()
-	clips.Save("rulez.clp")
+        clips.Run()
+        clips.Save("rulez.clp")
         exit()
 
     def client_open_directory(self):
-        #from  Tkinter import *
-        #import Tkinter, Tkconstants, tkFileDialog
-        #root = Tk()
-        #root.directory = tkFileDialog.askdirectory()
-        #print(root.directory)
+        # from  Tkinter import *
+        # import Tkinter, Tkconstants, tkFileDialog
+        # root = Tk()
+        # root.directory = tkFileDialog.askdirectory()
+        # print(root.directory)
         print("proba")
 
    # def show_frame(self, page_name):
@@ -340,45 +301,16 @@ class Window(Frame):
             subprocess.call([opener, filename])
 
 
-class Combo:
-    def __init__(self, parent):
-        self.parent = parent;
-        self.values = StringVar();
-        self.make_combobox(self.parent, self.values)
-        self.add_symptoms(self.values)
-
-    def make_combobox(self, parent, values):
-        newCombo = ttk.Combobox(self.parent, textvariable=self.values)
-        newCombo.pack(fill=BOTH, padx=20, pady=10, ipadx=5, ipady=5, side=TOP)
-        return newCombo
-
-    def add_symptoms(self, symptoms):
-        newCombo = ttk.Combobox(self.parent, textvariable=self.values)
-        newCombo['values'] = symptoms
-
-
-class Combos:
-    def __init__(self, parent):
-        self.parent = parent
-
-    def add_combo(self, symptom):
-        newCombo = ttk.Combobox(self.parent, textvariable=symptom)
-        newCombo.pack(fill=BOTH, padx=20, pady=10, ipadx=5, ipady=5, side=TOP)
-        # newCombo.grid(column=c, row=r)
-        # newCombo.current(0)
-        return newCombo
-
-
 class ParametersWin(Frame):
 
     def __init__(self, parent, ancestor):
         Frame.__init__(self, parent)
         self.ancestor = ancestor
-	self.values = ancestor.values
-	#print ancestor.values
+        self.values = ancestor.values
+        # print ancestor.values
         label = Label(self, text="Edit rules")
-	self.init_window()        
-	label.pack(side="top", fill="x", pady=10)
+        label.pack(side="bottom", fill="x", pady=10)
+        self.init_window()
 
     def init_window(self):
         # changing the title of our master widget
@@ -390,78 +322,82 @@ class ParametersWin(Frame):
         self.tabControl.add(self.tabAdd, text=' Add rule ')  # Add the tab
         self.tabEdit = Frame(self.tabControl) # Create a tab
         self.tabControl.add(self.tabEdit, text=' Edit rule ')  # Add the tab
-        #self.tabTwo = Frame(self.tabControl)  # Create a tab
-        #self.tabControl.add(self.tabTwo, text=' Monetary Incentive Delay ')  # Add the tab
-        self.tabControl.pack(side="top",expand=1, fill="both")  # Pack to make visible
+        # self.tabTwo = Frame(self.tabControl)  # Create a tab
+        # self.tabControl.add(self.tabTwo, text=' Monetary Incentive Delay ')  # Add the tab
+        self.tabControl.pack(side="top", expand=1, fill="both")  # Pack to make visible
 
-	################## TAB ADD
+        ################## TAB ADD
+
+        Label(self.master, text="Add new rule by entering the rule name, symptom and the malfunctions").pack()
+
         # entry to add malfunction
         self.e = Entry(self.tabAdd)
-        self.e.pack()#side="top", padx=50, pady=50)
-	self.LHS = Text(self.tabAdd,height=12, width=50)
-        self.LHS.pack()#x=0,y=0, height=200, width=200)
-	self.e.delete(0, END)
+        self.e.pack()
+        self.LHS = Text(self.tabAdd,height=12, width=50)
+        self.LHS.pack()
+        self.e.delete(0, END)
         self.e.insert(0, "add-rule-name")
 
         self.RHS = Text(self.tabAdd,height=12, width=50)
-        self.RHS.pack()#x=101,y=101, height=200, width=200)
+        self.RHS.pack()
 
-	self.but = Button(self.tabAdd, text='add', width=15, height=5,
+        self.but = Button(self.tabAdd, text='add', width=15, height=5,
                              command=lambda: self.add_rule(self.e.get(),self.LHS.get(0.0, END), self.RHS.get(0.0, END)))
         self.but.pack(side="bottom", padx=0, pady=0)
-	
-	self.RHS.insert(0.0, "add possible malfunction")
+
+        self.RHS.insert(0.0, "add possible malfunction")
         self.LHS.insert(0.0, "add sypmtom")
-	
-	################## TAB EDIT
-	#self.symptom = StringVar()
-        #self.combo_one = ttk.Combobox(self.tabEdit, textvariable=self.symptom, state='readonly')
-        #self.combo_one.configure(values=self.values)
-	#self.combo_one.pack()
-	#Load the whole file in a text
-	self.file = Text(self.tabEdit,height=28, width=65)
+
+        ################## TAB EDIT
+        # self.symptom = StringVar()
+        # self.combo_one = ttk.Combobox(self.tabEdit, textvariable=self.symptom, state='readonly')
+        # self.combo_one.configure(values=self.values)
+        # self.combo_one.pack()
+        # Load the whole file in a text
+        self.file = Text(self.tabEdit, height=28, width=65)
         self.file.pack()#x=101,y=101, height=200, width=200)
-	self.fileButton = Button(self.tabEdit,text="Edit",command= lambda: self.editFile(self.file.get(0.0, END)))
-	self.fileButton.pack()
-	f = open(self.ancestor.filename, 'r')
-	for line in f:
-		#print line
-		self.file.insert(END,line)
-	f.close()
-	
-    def editFile(self,textToSave):
-	f = open(self.ancestor.filename, 'w')
-	for line in textToSave:
-		f.write(line)
-	f.close()
-	clips.Clear()
-	clips.Load(self.ancestor.filename)
+        self.fileButton = Button(self.tabEdit, text="Edit", command=lambda: self.editFile(self.file.get(0.0, END)))
+        self.fileButton.pack()
+        f = open(self.ancestor.filename, 'r')
+        for line in f:
+            # print line
+            self.file.insert(END, line)
+        f.close()
+
+    def editFile(self, textToSave):
+        f = open(self.ancestor.filename, 'w')
+        for line in textToSave:
+            f.write(line)
+        f.close()
+        clips.Clear()
+        clips.Load(self.ancestor.filename)
 
 
     def add_rule(self,name, rule, things):
-        #print name,rule, things
-        #self.LHS.delete(0.0, END)
-        #self.RHS.delete(0.0, END)
-	#self.RHS.insert(0.0, "add possible malfunction")
-        #self.LHS.insert(0.0, "add sypmtom")
-	#dataLHS = self.LHS.get(0.0, END)
+        # print name,rule, things
+        # self.LHS.delete(0.0, END)
+        # self.RHS.delete(0.0, END)
+        # self.RHS.insert(0.0, "add possible malfunction")
+        # self.LHS.insert(0.0, "add sypmtom")
+        # dataLHS = self.LHS.get(0.0, END)
         split_rule = rule.splitlines()
-	#data = self.text_area.get(0.0, END)
+        # data = self.text_area.get(0.0, END)
         split_things = things.splitlines()
-	print name,split_rule,split_things
-	stringRHSL = '(assert ('
-	stringRHSR = '))'
-	stringLHSL = '('
-	stringLHSR = ')'
-	RHSString = ''
-	LHSString = ''
-	for i in range(len(split_rule)):
-		LHSString=LHSString+stringLHSL+split_rule[i].encode("utf-8")+stringLHSR
-	for i in range(len(split_things)):
-		RHSString=RHSString+stringRHSL+split_things[i].encode("utf-8")+stringRHSR
-	#clips.BuildRule("user-rule", "(test (eq (python-call py_getvar",RHSString, "the user rule")
-	clips.BuildRule(name, LHSString,RHSString, "the user rule")
-        pass
+        # print name, split_rule, split_things
+        stringRHSL = '(assert ('
+        stringRHSR = '))'
+        stringLHSL = '('
+        stringLHSR = ')'
+        RHSString = ''
+        LHSString = ''
+        for i in range(len(split_rule)):
+            LHSString = LHSString + stringLHSL + split_rule[i].encode("utf-8") + stringLHSR
+        for i in range(len(split_things)):
+            RHSString = RHSString + stringRHSL + split_things[i].encode("utf-8") + stringRHSR
+            # clips.BuildRule("user-rule", "(test (eq (python-call py_getvar",RHSString, "the user rule")
+            clips.BuildRule(name, LHSString, RHSString, "the user rule")
+            pass
+
 
 if __name__ == "__main__":
     # root window created. Here, that would be the only window, but
@@ -469,6 +405,7 @@ if __name__ == "__main__":
     root = Tk()
 
     root.geometry("600x500")
+    root.resizable(0, 0)
 
     # creation of an instance
     app = Window(root)
